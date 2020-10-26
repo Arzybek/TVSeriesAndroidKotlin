91a335ac6b95223c9936f8e9473b5f4b451eb5e1
Zakepanka <arzybek@users.noreply.github.com>
2020-10-26 20:08:10 +0500

    Test

--- app/src/main/java/com/example/tvseriesprojectapp/MainActivity.kt
+++ app/src/main/java/com/example/tvseriesprojectapp/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.tvseriesprojectapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val layout = findViewById<ConstraintLayout>(R.id.root)
+
+        val textView = TextView(this)
+        //setting height and width
+        textView.layoutParams = ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        textView.setText("GEEKSFORGEEKS")
+        layout?.addView(textView)
     }
 }
