dfb220548b96a12e1bbf854800c7601164303204
Арзыбек Казикулов <Arzybek>
2020-11-05 20:35:35 +0500

    Refactored code

--- app/src/main/java/com/example/tvseriesprojectapp/MainActivity.kt
+++ app/src/main/java/com/example/tvseriesprojectapp/MainActivity.kt
@@ -12,8 +12,13 @@ import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import kotlinx.coroutines.experimental.launch
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+    val ip = "192.168.0.103"
+    val port = "8080"
+    val gson = Gson();
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,24 +26,26 @@ class MainActivity : AppCompatActivity() {
         val layout = findViewById<LinearLayout>(R.id.root)
 
         var listOfShows: MutableList<TvShow> = mutableListOf();
-        val url = "http://localhost:8080/tvshows"
-        val queue = Volley.newRequestQueue(this)
-        val textView = TextView(this)
-        textView.layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-        val gson = Gson();
-        var err: String = "";
-        var resp: String = "";
-        val stringRequest = StringRequest(
-            Request.Method.GET, url,
-            Response.Listener<String> { response -> 
-                textView.text = response },
-            Response.ErrorListener { response -> textView.text = "${response.message}" }
-            )
-        queue.add(stringRequest)
-//        mock
-        val str: String = "[{\"id\":1,\"name\":\"Everybody hates Chris\",\"category\":\"comedy\",\"year\":2005},{\"id\":2,\"name\":\"Friends\",\"category\":\"comedy\",\"year\":1994}]";
-        listOfShows = gson.fromJson(str, Array<TvShow>::class.java).toMutableList()
+        val url = "http://${ip}:${port}/tvshows"
 
+        var resp: String = "";
+//      val str: String = "[{\"id\":1,\"name\":\"Everybody hates Chris\",\"category\":\"comedy\",\"year\":2005},{\"id\":2,\"name\":\"Friends\",\"category\":\"comedy\",\"year\":1994}]";
+
+//        val err = ""
+//        val queue = Volley.newRequestQueue(this)
+//        val stringRequest = StringRequest(
+//            Request.Method.GET, url,
+//            Response.Listener<String> { response -> resp = response },
+//            Response.ErrorListener { response -> err = "${response.message}" }
+//            )
+//        queue.add(stringRequest)
+
+        launch {
+            resp = URL(url).readText()
+        }
+        Thread.sleep(1000)
+
+        listOfShows = gson.fromJson(resp, Array<TvShow>::class.java).toMutableList()
         for (one in listOfShows) {
             val textView = TextView(this)
             textView.layoutParams = LinearLayout.LayoutParams(
